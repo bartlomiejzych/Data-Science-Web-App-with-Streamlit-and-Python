849d402b333584277ebb9b541bf9b757a6bc3171
Bartłomiej Zych <bartlomiejzych98@gmail.com>
2021-08-25 23:11:27 +0200

    Added a chart using plotly

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import plotly.express as px
 
 DATA_URL = (
     'D:\Coursera\Motor_Vehicle_Collisions_-_Crashes.csv'
@@ -55,6 +56,14 @@ st.write(pdk.Deck(
     ],
 ))
 
+st.subheader('Breakdown by minute between %i:00 and %i:00' % (hour, (hour + 1) % 24))
+filtered = data[
+    (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour+1))
+]
+hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0, 60))[0]
+chart_data = pd.DataFrame({'minute': range(0, 60), 'crashes':hist})
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
+st.write(fig)
 
 if st.checkbox('Show Raw Data', False):
     st.subheader('Raw Data')
